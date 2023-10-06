d3863ff3b823faa8cef26bdc21d8346f521ec9ca
ahmed sami <ahmed.sami@vaimanagement.co>
2023-10-06 16:53:05 +0400

    basic table

---
+++ src/components/BasicTable.jsx
@@ -0,0 +1,76 @@
+import { useMemo } from 'react'
+
+// Hooks
+import { useTable } from 'react-table'
+
+// Data
+import MOCK_DATA from './MOCK_DATA.json'
+import { COLUMNS } from './columns'
+
+// Style
+import Style from './BasicTable.module.css'
+
+const BasicTable = () => {
+  // memoization for the data
+  const columns = useMemo(() => COLUMNS, [])
+  const data = useMemo(() => MOCK_DATA, [])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  })
+
+  return (
+    <div className={Style.wrapper}>
+      <table className={Style.customers} {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup, index) => (
+            <tr key={index} {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column, index) => (
+                <th key={index} {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, index) => {
+            prepareRow(row)
+            return (
+              <tr key={index} {...row.getRowProps()}>
+                {row.cells.map((cell, index) => {
+                  return (
+                    <td key={index} {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map((footerGroup, index) => (
+            <tr key={index} {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map((column, index) => (
+                <td key={index} {...column.getFooterProps()}>
+                  {column.render('Footer')}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </div>
+  )
+}
+
+export default BasicTable
